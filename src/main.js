bb183c13b9df9ba8fb138555133190264d9aeecc
shree <shenruiyun@nbeport.com>
2019-11-20 21:32:18 +0800

    123

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ import Element from 'element-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { mockXHR } from '../mock'
 
+import * as filters from './filters' // global filters
+
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
 Vue.use(Element, {
   size: 'medium'
   // i18n: (key, value) => i18n.t(key, value)
 })
+
+// 全局添加格式化规则
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
 Vue.config.productionTip = false
 
 new Vue({
